9f7464326e4c8df09c030077451737d5594245fa
hehepeng <cnmsbqnm@gmail.com>
2022-08-03 22:20:12 +0800

    完成数据源测试连接功能，修复了填写数据源地址和端口时，jdbcUrl不变的bug

--- src/views/report-database/index.vue
+++ src/views/report-database/index.vue
@@ -11,20 +11,19 @@
                        @select="selectDatabase"
                        v-model="type"
                        placeholder="数据库类型" />
-      <el-button type="primary"
-                 plain>查询</el-button>
+      <el-button type="primary" plain>查询
+      </el-button>
     </div>
 
     <div class="body">
       <div class="table-operate">
-        <el-button @click.stop="visible = true">新增</el-button>
+        <el-button @click.stop="visible = true">新增
+        </el-button>
         <el-button type="danger">批量删除</el-button>
       </div>
       <div class="clearfix"></div>
-      <el-table border
-                :data="table">
-        <el-table-column prop="id"
-                         label="id" />
+      <el-table border :data="table">
+        <el-table-column prop="id" label="id" />
         <el-table-column prop="databaseName"
                          label="数据库" />
         <el-table-column prop="businessName"
@@ -41,8 +40,7 @@
         </el-table-column>
       </el-table>
 
-      <el-pagination class="pagination"
-                     background
+      <el-pagination class="pagination" background
                      @size-change="handleSizeChange"
                      @current-change="handleCurrentChange"
                      :current-page="pageInfo.pageIndex"
@@ -51,39 +49,34 @@
                      layout="total, sizes, prev, pager, next, jumper"
                      :total="pageInfo.total" />
     </div>
-    <el-dialog class="dialog"
-               title="新增"
+    <el-dialog class="dialog" title="新增"
                :visible.sync="visible">
       <el-input class="title-input"
                 v-model="form.businessName"
                 placeholder="数据库业务名" />
-      <el-form :model="form"
-               :rules="rules"
-               ref="ruleForm"
-               label-width="120px"
-               label-position="left"
-               inline>
+      <el-form :model="form" :rules="rules"
+               ref="ruleForm" label-width="120px"
+               label-position="left" inline>
         <el-form-item label="数据库"
                       prop="databaseName">
           <el-input class="form-input"
                     v-model="form.databaseName" />
         </el-form-item>
-        <el-form-item label="数据库类型"
-                      prop="typeLabel">
-          <el-autocomplete class="form-input"
-                           :fetch-suggestions="querySearch"
-                           value-key="label"
-                           @select="selectType"
-                           v-model="form.typeLabel"
-                           placeholder="数据库类型" />
+        <el-form-item label="数据库类型" prop="type">
+          <el-select v-model="form.type"
+                     class="form-input">
+            <el-option v-for="(item, index) of options"
+                       :key="index"
+                       :label="item.label"
+                       :value="item.value">
+            </el-option>
+          </el-select>
         </el-form-item>
-        <el-form-item label="数据库地址"
-                      prop="url">
+        <el-form-item label="数据库地址" prop="url">
           <el-input class="form-input"
                     v-model="form.url" />
         </el-form-item>
-        <el-form-item label="数据库端口"
-                      prop="port">
+        <el-form-item label="数据库端口" prop="port">
           <el-input class="form-input"
                     v-model.number="form.port" />
         </el-form-item>
@@ -99,36 +92,29 @@
         </el-form-item>
       </el-form>
 
-      <textarea type="textarea"
-                class="url-tag"
-                v-model="jdbcUrl"
-                readonly />
+      <textarea type="textarea" class="url-tag"
+                v-model="form.jdbcUrl" readonly />
 
-      <span slot="footer"
-            class="dialog-footer">
-        <el-button @click="visible = false">取 消</el-button>
-        <el-button type="primary"
-                   @click="submit">确 定</el-button>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="visible = false">取 消
+        </el-button>
+        <el-button type="warning"
+                   @click="connectionTest">测试 连接
+        </el-button>
+        <el-button type="primary" @click="submit">
+          确 定</el-button>
       </span>
     </el-dialog>
   </section>
 </template>
 
 <script>
-import { addDataSource, getDataSourcesByKeyword } from '@/apis/index'
+import { addDataSource, getDataSourcesByKeyword, connectionTest } from '@/apis/index'
 export default {
   data () {
     const databaseCheck = (rule, value, callback) => {
       if (!/(^[a-z A-Z]+$)|(_-)/.test(value)) {
         return callback(new Error('只能输入英文字符'))
-      }
-
-      callback()
-    }
-
-    const typeCheck = (rule, value, callback) => {
-      if (!['MySQL', 'sql-server', 'oracle'].includes(value)) {
-        return callback(new Error('数据库类型不支持'))
       }
 
       callback()
@@ -152,9 +138,9 @@ export default {
 
     return {
       jdbcUrlMap: {
-        MySQL: 'jdbc:mysql://localhost:port/databaseName?useUnicode=true&characterEncoding=UTF-8&useSSL=false&autoReconnect=true',
-        Oracle: 'jdbc:oracle:thin:@localhost:port:databaseName',
-        SqlServer: 'jdbc:sqlserver://localhost:port;databaseName=databaseName'
+        0: 'jdbc:mysql://localhost:port/databaseName?useUnicode=true&characterEncoding=UTF-8&useSSL=false&autoReconnect=true',
+        1: 'jdbc:oracle:thin:@localhost:port:databaseName',
+        2: 'jdbc:sqlserver://localhost:port;databaseName=databaseName'
       },
       options: [{
         label: 'MySQL',
@@ -163,7 +149,7 @@ export default {
         label: 'SqlServer',
         value: 1
       }, {
-        label: 'oracle',
+        label: 'Oracle',
         value: 2
       }],
       type: '',
@@ -173,9 +159,9 @@ export default {
       form: {
         databaseName: '',
         businessName: '',
-        typeLabel: 'MySQL',
         type: 0,
         url: '',
+        jdbcUrl: '',
         port: '',
         username: '',
         password: ''
@@ -186,12 +172,7 @@ export default {
         }, {
           required: true, message: '数据库不能为空', trigger: 'blur'
         }],
-        businessName: [{
-          required: true, message: '业务名不能为空', trigger: 'blur'
-        }],
-        typeLabel: [{
-          validator: typeCheck, trigger: 'change'
-        }, {
+        type: [{
           required: true, message: '类型不能为空', trigger: 'blur'
         }],
         url: [{
@@ -219,27 +200,30 @@ export default {
       }
     }
   },
-  computed: {
-    jdbcUrl () {
-      if (!this.form.typeLabel) {
-        return this.jdbcUrlMap[this.form.typeLabel]
-      }
-
-      if (!this.form.url && !this.form.port && !this.form.databaseName) {
-        return this.jdbcUrlMap[this.form.typeLabel]
-      }
-
-      if (this.form.url) {
-        return this.jdbcUrlMap[this.form.typeLabel].replace(/localhost/, this.form.url)
-      }
-
-      if (this.form.port) {
-        return this.jdbcUrlMap[this.form.typeLabel].replace(/port/, this.form.port)
-      }
-
-      if (this.form.databaseName) {
-        return this.jdbcUrlMap[this.form.typeLabel].replace(/databaseName/, this.form.databaseName)
-      }
+  watch: {
+    'form.type': {
+      handler (newVal) {
+        this.form.jdbcUrl = this.jdbcUrlMap[newVal]
+      },
+      immediate: true
+    },
+    'form.url': {
+      handler (newVal) {
+        this.form.jdbcUrl = this.jdbcUrlMap[this.form.type].replace(/localhost/, newVal ? newVal : 'localhost').replace(/port/, this.form.port ? this.form.port : 'port').replace(/databaseName/, this.form.databaseName ? this.form.databaseName : 'databaseName')
+      },
+      immediate: true
+    },
+    'form.port': {
+      handler (newVal) {
+        this.form.jdbcUrl = this.jdbcUrlMap[this.form.type].replace(/localhost/, this.form.url ? this.form.url : 'localhost').replace(/port/, newVal ? newVal : 'port').replace(/databaseName/, this.form.databaseName ? this.form.databaseName : 'databaseName')
+      },
+      immediate: true
+    },
+    'form.databaseName': {
+      handler (newVal) {
+        this.form.jdbcUrl = this.jdbcUrlMap[this.form.type].replace(/localhost/, this.form.url ? this.form.url : 'localhost').replace(/port/, this.form.port ? this.form.port : 'port').replace(/databaseName/, newVal ? newVal : 'databaseName')
+      },
+      immediate: true
     }
   },
   created () {
@@ -247,7 +231,7 @@ export default {
   },
   methods: {
     querySearch (queryString, cb) {
-      var results = queryString ? this.options.filter(this.createFilter(queryString)) : this.options
+      const results = queryString ? this.options.filter(this.createFilter(queryString)) : this.options
       // 调用 callback 返回建议列表的数据
       cb(results)
     },
@@ -259,10 +243,6 @@ export default {
     selectDatabase (val) {
       // debugger
       this.database = val.label
-    },
-    selectType (val) {
-      this.form.typeLabel = val.label
-      this.form.type = val.value
     },
     // 当前页的记录数
     handleSizeChange (val) {
@@ -311,6 +291,24 @@ export default {
       }
 
       addDataSource(params)
+    },
+    connectionTest () {
+      this.$refs['ruleForm'].validate((valid) => {
+        if (valid) {
+          const params = {
+            username: this.form.username,
+            password: this.form.password,
+            jdbcUrl: this.form.jdbcUrl,
+            type: this.form.type
+          }
+
+          connectionTest(params)
+        } else {
+          console.log('error submit!!')
+          return false
+        }
+      })
+
     },
     submit () {
       if (!this.form.businessName) return this.$message.error('业务名不能为空')
