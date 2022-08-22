2f3f93c8e460e1aeb14476a758253cd46812f998
hehepeng <18846042826@163.com>
2022-08-22 18:09:36 +0800

    解析compressedContent

--- src/views/book-reader/index.vue
+++ src/views/book-reader/index.vue
@@ -17,33 +17,66 @@
         </div>
 
         <div class="icon-wrapper flex-row main-center secondary-center">
-          <span class="icon-font icon">A</span>
+          <span class="icon-font icon"></span>
         </div>
       </div>
       <drawer :visible.sync="themeVisible">
-        <div class="theme flex-row secondary-center">
+        <div class="theme flex-column">
+          <label class="theme-label">视图模式</label>
+
+          <div class="drawer-items">
+            <el-tooltip class="item"
+                        effect="dark"
+                        content="单页模式"
+                        placement="top">
+              <i class="icon-single-page mode-icon"
+                 @click.stop="handleSpread('single')"></i>
+            </el-tooltip>
+
+            <el-tooltip class="item"
+                        effect="dark"
+                        content="双页模式"
+                        placement="top">
+              <i class="icon-double-page mode-icon"
+                 @click.stop="handleSpread('double')"></i>
+            </el-tooltip>
+
+            <el-tooltip class="item"
+                        effect="dark"
+                        content="滚动模式"
+                        placement="top">
+              <i class="icon-scroll-page mode-icon"
+                 @click.stop="handleSpread('scroll')"></i>
+            </el-tooltip>
+          </div>
+        </div>
+        <div class="theme flex-column">
           <label class="theme-label">背景</label>
 
-          <radio radio-type="theme"
-                 v-for="(item, index) of bgColorOptions"
-                 :key="item.label"
-                 color="#708090"
-                 :label="item.label"
-                 v-model="themeColor"
-                 @change="selectTheme(index)" />
+          <div class="drawer-items">
+            <radio radio-type="theme"
+                   v-for="(item, index) of bgColorOptions"
+                   :key="item.label"
+                   color="#708090"
+                   size="large"
+                   :label="item.label"
+                   v-model="themeColor"
+                   @change="selectTheme(index)" />
+          </div>
         </div>
       </drawer>
     </section>
     <div class="reader-wrapper">
       <div id="read"></div>
+      <div class="bg-one"></div>
+      <div class="bg-two">
+        <div class="spine-shadow-left"></div>
+        <div class="book-spine"></div>
+        <div class="spine-shadow-right"></div>
+      </div>
+      <div class="bg-three"></div>
     </div>
     <div class="operate flex-row main-between">
-      <!-- frameborder属性：不设置边框，如果是vue2.x版本，那么可以使用src属性 -->
-      <!-- <iframe style="width: 100%; height: 800px;"
-              id="tempHtml"
-              ref="tempHtml"
-              frameborder="0"></iframe> -->
-
       <el-button @click="prev">上一页</el-button>
 
       <el-button @click="next">下一页</el-button>
@@ -70,7 +103,8 @@
 import Epub from 'epubjs'
 import turn from '@/utils/turn.js'
 
-const DOWNLOAD_URL = '/static/81642.mobi'
+const DOWNLOAD_URL = 'http://101.35.44.70:9000/file/%E4%B8%89%E5%9B%BD%E6%BC%94%E4%B9%89.epub'
+const fastTextEncoding = require('fast-text-encoding')
 
 export default {
   name: 'BookReader',
@@ -119,6 +153,7 @@ export default {
   },
   mounted () {
     this.showEpub()
+    // this.onTurn()
   },
   watch: {
     themeColor (newVal) {
@@ -135,74 +170,35 @@ export default {
       const { toc } = await this.book.loaded.navigation
       this.bookMenus = toc
 
-
-      // Book对象的钩子函数ready
-      this.book.ready.then(async () => {
-        // 执行分页
-        return this.book.locations.generate()
-      }).then(result => {
-        // 获取Locations对象
-        this.locations = this.book.locations
-      })
+      console.log('book: ', this.book)
+      const { zip } = this.book.archive
+      let utf8decoder = new TextDecoder()
+      console.log('zip: ', zip.files)
+      for (let i = 0; i < Object.values(zip.files).length; i++) {
+        // console.log('compress: ', Object.values(zip.files)[i]._data.compressedContent)
+        console.log('utf8decoder: ', utf8decoder.decode(Object.values(zip.files)[i]._data.compressedContent))
+      }
+      // for (let i of zip.files) {
+      //   console.log('compress: ', zip.files[i])
+      // }
 
       // 生成Rendition,通过Book.renderTo生成
       this.rendition = this.book.renderTo('read', {
-        width: '100%',
+        width: '600px',
         height: window.innerHeight - 100,
         allowScriptedContent: true
-      })
-
-      // 向图书iframe框架中，加载字体
-      this.rendition.hooks.content.register(contents => {
-        Promise.all([
-          contents.addStylesheet(
-            'https://fonts.font.im/css?family=Hanalei+Fill|Kirang+Haerang|Merriweather|MedievalSharp|Ranga')
-        ]).then(() => {
-          // console.log('字体全部加载完毕。。。')
-        })
       })
 
       this.registerThemes()
 
       // 通过Rendition.display渲染电子书
       this.rendition.display()
-    },
-    // 解析电子书内容，获取封面，标题，作者等信息
-    parseBook () {
-      // 获取cover
-      this.book.loaded.cover.then(cover => {
-        this.book.archive.createUrl(cover).then(url => {
-          this.setCover(url)
-        })
-      })
 
-      // 标题作者
-      this.book.loaded.metadata.then(metadata => {
-        this.setMetadata(metadata)
-      })
-
-      // 目录信息
-      this.book.loaded.navigation.then(nav => {
-        // flatten--扁平化处理，三维数组变一维数组
-        const navItem = this.flatten(nav.toc)
-        // 判断当前目录是第几级目录
-        function find (item, v = 0) {
-          const parent = navItem.filter(it => it.id === item.parent)[0]
-          return !item.parent ? v : (parent ? find(parent, ++v) : v)
-        }
-        navItem.forEach(item => {
-          item.level = find(item)
-        })
-        // BUG 这里没问题，navigation目录中没有目录-和作者信息，二section跳转时候，会有他们，所以会差两节
-        // 用老师的书，没有问题，我自己写的书有这个问题(中英文的问题？、我下的书没有完全符号epub标准？)
-        this.setNavigation(navItem)
-      })
     },
     toMenu (item) {
+      console.log('item: ', item)
       this.rendition.display(item.href)
-      // this.menusVisible = false
-
-      console.log('pageCount: ', this.book.pageList.item)
+      this.menusVisible = false
     },
     prev () {
       // Rendition.prev
@@ -225,9 +221,11 @@ export default {
         this.rendition.themes.register(theme.name, theme.style)
       })
     },
-    // 打散目录数据，将三维数组转变为一维数组
-    flatten (array) {
-      return [].concat(...array.map(v => [v, ...this.flatten(v.subitems)]))
+    // 设置阅读器分页模式
+    handleSpread (mode) {
+      if (mode === 'single') { }
+      if (mode === 'double') { }
+      if (mode === 'scroll') { }
     }
   }
 }
@@ -238,6 +236,95 @@ export default {
   position: relative;
   height: 100%;
   .reader-wrapper {
+    position: relative;
+    #read {
+      position: relative;
+      // display: flex;
+      // justify-content: center;
+    }
+    // @media (min-width: 1000px) {
+    //   #read:after {
+    //     position: absolute;
+    //     width: 1px;
+    //     border-right: 2px #000 solid;
+    //     height: 90%;
+    //     z-index: 1;
+    //     left: 50%;
+    //     margin-left: -1px;
+    //     top: 5%;
+    //     opacity: 0.15;
+    //     box-shadow: -2px 0 15px rgba(0, 0, 0, 1);
+    //     content: '';
+    //   }
+    // }
+    .bg-one,
+    .bg-two,
+    .bg-three {
+      box-shadow: 0px 0px 2px rgba(0, 0, 0, 0.18);
+    }
+    .bg-one {
+      position: absolute;
+      left: 7px;
+      right: 7px;
+      top: 5px;
+      bottom: 5px;
+      border: 1px solid rgba(191, 191, 191, 1);
+      z-index: 1;
+    }
+    .bg-two {
+      position: absolute;
+      left: 9px;
+      right: 9px;
+      top: 5px;
+      bottom: 5px;
+      border: 1px solid rgba(191, 191, 191, 1);
+      z-index: 2;
+      overflow: hidden;
+      .spine-shadow-left {
+        width: 68px;
+        height: 100%;
+        background: url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAEQAAAADCAYAAAAqTqlmAAAA/0lEQVQoke2SQY7CMAxF/3dUUfUCbLj/kViz4AAghGgS238WQyOq4QhjKXqxZcfOlykJAAwAv53r9cppmgwASimD9/udZkYzI0k+n89xr7UaANRabVkWbH7v3WqtNDO21rgsC1trdHcDgIj4Q3cnSb5eL5oZ3X1QkgFAa80AIDMtMxkRjAiaGSOCmcnMpCRzdwAYBAB3ty1nE0L4NQHA+XwGAFwuF5xOJ2SmjscjJOl2u1FvFTNTAPB4PPBpW3yeZ63rSkmqtXKLHw4HzfOMdV13+Zmp3js/CQCSNE3TeL/3vqsrpSgixlylFL0/+nWuzJSZMTP1FmL04f+G7DfkB/uZQX9OFIuVAAAAAElFTkSuQmCC)
+          repeat-y;
+        z-index: 10;
+        margin: 0;
+        z-index: 10;
+        position: absolute;
+        left: calc(50% - 68px);
+      }
+      .book-spine {
+        position: absolute;
+        width: 1px;
+        height: 100%;
+        position: absolute;
+        left: 50%;
+        bottom: 100%;
+        bottom: 0;
+        border: 2px solid rgba(191, 191, 191, 1);
+        border-left: none;
+        box-sizing: border-box;
+        z-index: 10;
+      }
+      .spine-shadow-right {
+        width: 68px;
+        height: 100%;
+        background: url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAEQAAAADCAYAAAAqTqlmAAABVklEQVQokW1Suw4iMQycyWMThBB0NFtQ8yf3WfwkNQUNHRLSKquwnitus4pOWIrGHnlsJzFvt9sf55yccwohGP6ZhRA2JGktx3svM5P3Xt57rbwBwDAMBgAkLYQgM1PD3W4nSQohyHtvAPA/hhBsGAZN06RhGGRmSikpxmgAkFKyaZqQUtpiSTIz7fd7Nd/MdDwetSyLAUDDWquN4ygAPw9Jc5IEAGshmpmcczSzjUdn3+8X6yUEAJK0LAtb3PJrrZsmxoi+T62VPfa6Ugpyzkgpbfw8z5SkUgpzzj/n+nw+6HlJer/flKTz+Qwz0ziOeDweAID7/d6krY4k0ZEkADjnuP729iiN7xuvm4NlWbhuA733anHLjzFumlor+j4xRvXY63LOKKVgnueNTymJJHPOKqX8nOtwOKDnSfJ0OokkX68XnHN8Pp+4XC4AgOv12qStDknqLwxUBhX5L/TkAAAAAElFTkSuQmCC)
+          repeat-y;
+        z-index: 10;
+        margin: 0;
+        box-sizing: border-box;
+        position: absolute;
+        right: calc(50% - 68px);
+      }
+    }
+    .bg-three {
+      position: absolute;
+      left: 5px;
+      right: 5px;
+      top: 5px;
+      bottom: 5px;
+      border: 1px solid rgba(191, 191, 191, 1);
+      z-index: 0;
+    }
   }
   .operate {
     position: absolute;
@@ -299,6 +386,23 @@ export default {
     padding: 20px;
     .theme-label {
       margin-right: 10px;
+      font-size: 15px;
+    }
+
+    .drawer-items {
+      margin-top: 10px;
+      .mode-icon {
+        font-size: 60px;
+        cursor: pointer;
+        opacity: 0.4;
+        margin: 0 10px;
+        &:first-of-type {
+          margin-left: 0;
+        }
+      }
+      .icon-double-page {
+        font-weight: bold;
+      }
     }
   }
 }
