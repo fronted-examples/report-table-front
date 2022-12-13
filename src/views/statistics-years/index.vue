071fb25f846246054e8440fd1690222066e3e570
hehepeng <18846042826@163.com>
2022-12-13 14:26:14 +0800

    fix that

--- src/views/statistics-years/index.vue
+++ src/views/statistics-years/index.vue
@@ -21,15 +21,14 @@ export default {
   },
   methods: {
     initWebSocket: function () {
-      let that = this
       // 建立连接对象
-      that.socket = new SockJS("http://localhost:1112/api")
+      this.socket = new SockJS("http://localhost:1112/api")
       // 获取STOMP子协议的客户端对象
-      that.stompClient = Stomp.over(that.socket)
-      that.stompClient.debug = false
-      that.stompClient.reconnect_delay = 5000
+      this.stompClient = Stomp.over(this.socket)
+      this.stompClient.debug = false
+      this.stompClient.reconnect_delay = 5000
       // 建立连接
-      that.stompClient.connect({}, (frame) => {
+      this.stompClient.connect({}, (frame) => {
         // 订阅主题
         this.stompClient.subscribe('/topic/all', res => {
           console.info('订阅主题: ', res)
@@ -39,9 +38,10 @@ export default {
             duration: 3000
           })
         })
-        // 订阅点对点消息 username=111111(对应后端代码 simpMessagingTemplate.convertAndSendToUser("111111","/queue/cmdFinish","今天10点开项目启动会-linxin");)
+        // 订阅点对点消息 username=111111(对应后端代码 simpMessagingTemplate.convertAndSendToUser("111111","/cmdFinish","今天10点开项目启动会-linxin");)
         this.stompClient.subscribe(`/queue/${this.username}/cmdFinish`, res => {
           console.info('订阅点对点消息: ', res)
+
           this.$message({
             message: res.body,
             type: 'success',
